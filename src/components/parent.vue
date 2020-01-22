8ea902e2373855318843f97f3e573ac660422c0f
Nuno Caiadas <nuno.caiadas@mercedes-benz.io>
2020-01-22 18:22:51 +0000

    added firebase

---
+++ src/components/parent.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="parent-container">hello</div>
+</template>
+
+<script>
+export default {
+  name: 'parent',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.parent-container {
+  width: 400px;
+  height: 400px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: blue;
+  color: white;
+}
+</style>
---
+++ src/views/homepage.vue
@@ -0,0 +1,23 @@
+<template>
+  <parent></parent>
+
+</template>
+
+<script>
+import parent from '../components/parent';
+
+export default {
+  components: {
+    'parent': parent,
+  },
+  name: 'homepage',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
