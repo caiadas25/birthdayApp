6e9b91c98b576960105c543fa98b1b6a55707f5b
Nuno Caiadas <nuno.caiadas@mercedes-benz.io>
2020-02-04 11:54:58 +0000

    fetched data from firebase

--- src/components/person.vue
+++ src/components/person.vue
@@ -1,89 +1,82 @@
 <template>
-  <div class="person-container">
-      Closest birthday is: {{this.getClosestDate()}}
-    <div class="person" v-for= "person in people" :key="person.id">
-      {{person.birthDay}}
-      {{person.birthMonth}}
-    <router-link :to="{name: 'viewPerson', params: {name: person.name}}">
-      <div>
-        Ver pessoa
-      </div>
-    </router-link>
-    </div>
+  <div class="person-container"
+      v-if="!(parsedDates.length === 0)">
+      {{this.getClosestDate(parsedDates)}}
+      {{this.people}}
   </div>
 </template>
 <script>
 import db from '../firebase/firebaseInit.js';
+import { database, ref } from '../firebase/firebaseInit';
+import { errData, obtainData } from '../firebase/firebaseInit';
+
 
 export default {
   name: 'person',
   methods: {
     dateString() {
-      return 'birthMonth';
-    },
-    getData(){
-      db.collection('people').orderBy(this.dateString()).get().then(querySnapshot => {
-        querySnapshot.forEach(doc => {
-          const data = {
-            'id': doc.id,
-            'name': doc.data().name,
-            'birthDay': doc.data().birthDay,
-            'birthMonth': doc.data().birthMonth,
-            'birthYear': doc.data().birthYear,
-            'date': doc.data().date,
-          }
-          this.people.push(data);
-          let formattedBirthday = doc.data().birthDay + '/' + doc.data().birthMonth;
-          return data;
-        })
-      })
+      return 'name';
     },
 
-    getDatesAsMiliseconds(){
-
-      let dates = ["05/01", "01/01", "27/01", "22/12", "12/11", "03/04", "10/02", "08/08",].map(s => {
+    getDatesAsMiliseconds(firebaseData){
+      //console.log(firebaseData)
+      let dates = (firebaseData).map(s => {
         let datesAsMiliseconds = (moment(s, "DD/MM")._d.getTime()) - Date.now();
-        //console.log(datesAsMiliseconds)
         return datesAsMiliseconds;
       });
       let datesAsMiliseconds = dates;
       return datesAsMiliseconds;
     },
 
-    getDaysInTheFuture() {
-      let datesAsMiliseconds = this.getDatesAsMiliseconds();
+    getDaysInTheFuture(datesAsMiliseconds) {
+      //let datesAsMiliseconds = this.getDatesAsMiliseconds();
       function getPositiveNumbers(array) {
         return array.filter(value => value > 0);
       }
       let daysInTheFuture = getPositiveNumbers(datesAsMiliseconds);
       return daysInTheFuture;
     },
-    convertMilisecondsToDate() {
-      let datesInFutureInMiliseconds = this.getDaysInTheFuture();
+
+    convertMilisecondsToDate(datesInFutureInMiliseconds) {
+      // let datesInFutureInMiliseconds = this.getDaysInTheFuture();
       const converted = datesInFutureInMiliseconds.map(n => n + Date.now());
       const sorted = converted.sort();
       const parsed = sorted.map(n => moment(n).format("DD/MM/YYYY"));
-      console.log(parsed)
       return parsed;
     },
-    getClosestDate() {
-      //console.log(this.convertMilisecondsToDate()[0]);
-      return this.convertMilisecondsToDate()[0];
+
+    getClosestDate(parsedDates) {
+      // return this.convertMilisecondsToDate()[0];
+      //console.log(parsedDates, parsedDates[0]);
+      return parsedDates[0];
     }
   },
 
   data () {
     return {
-      people: []
+      parsedDates: [],
+      people: [],
     }
   },
   created() {
-    this.getDatesAsMiliseconds();
-    this.getData();
-    this.getDaysInTheFuture();
-    this.convertMilisecondsToDate();
-    this.getClosestDate();
+    obtainData()
+      .then(firebaseData => this.people = firebaseData)
+      .then(firebaseData => this.getDatesAsMiliseconds(firebaseData))
+      .then(datesAsMiliseconds => this.getDaysInTheFuture(datesAsMiliseconds))
+      .then(datesInFutureInMiliseconds => this.convertMilisecondsToDate(datesInFutureInMiliseconds))
+      .then(parsedDates => this.parsedDates = parsedDates);
+      // .then(parsedDates => this.getClosestDate(parsedDates));
+     // this.getClosestDate();
   },
+
+  //async created2() {
+  //  const firebaseData = await obtainData2();
+  //  const datesAsMiliseconds = this.getDatesAsMiliseconds(firebaseData);
+  //  const datesInFutureInMiliseconds = this.getDaysInTheFuture(datesAsMiliseconds);
+  //  const parsedDates = this.convertMilisecondsToDate(datesInFutureInMiliseconds);
+
+  //  this.parsedDates = parsedDates;
+  //}
 }
 </script>
 
--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -1,7 +1,34 @@
 import firebase from 'firebase';
-import 'firebase/firestore';
 import firebaseConfig from './firebaseConfig';
-
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-export default firebaseApp.firestore()
+var database = firebase.database();
+var ref = firebase.database().ref("people");
+
+
+function obtainData() {
+  return new Promise((resolve, reject) => {
+    let childDataArray = [];
+    ref.once("value", function(s) {
+      s.forEach(function(childSnapshot) {
+       var childData = childSnapshot.val();
+       var name = childData.name;
+       let birthDay = childData.birthDay;
+       let birthMonth = childData.birthMonth;
+       let parsed = birthDay + '/' + birthMonth + ': ' + name;
+       childDataArray.push(parsed);
+      });
+      console.log(childDataArray)
+      resolve(childDataArray);
+     });
+  });
+}
+
+function errData(err){
+  console.log(err);
+}
+
+
+export {database, ref, obtainData};
+export {errData};
+export default firebaseApp.firestore();
